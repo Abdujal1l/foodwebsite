1b0c8407c325f5439e3b22160b8ac8e8839c70d2
Abdujalil <olimjonov309@gmail.com>
2023-04-13 17:48:09 +0200

    dot navigation of slider

--- js/main.js
+++ js/main.js
@@ -434,6 +434,7 @@ window.addEventListener('DOMContentLoaded' , ()=>{
     
         //karusel
         const slides = document.querySelectorAll('.offer__slide'),
+              slider = document.querySelector('.offer__slider'), //94
               previousSlide = document.querySelector('.offer__slider-prev'),
               nextSlide = document.querySelector('.offer__slider-next');
         const totalSlidesNum = document.querySelector('#total'),
@@ -464,6 +465,49 @@ window.addEventListener('DOMContentLoaded' , ()=>{
             slide.style.width = width;
         });
 
+        slider.style.position = 'relative'; //94
+        const indicators = document.createElement('ol'); //94
+        const dots = [];
+        indicators.classList.add('carusel-indicators'); //94
+        indicators.style.cssText = `position: absolute; 
+                                    right: 0;
+                                    bottom: 0;
+                                    left: 0;
+                                    z-index: 15;
+                                    display: flex;
+                                    justify-content: center;
+                                    margin-right: 15%;
+                                    margin-left: 15%;
+                                    list-style: none;` ; //94
+
+        slider.append(indicators); //94
+
+        for(let i = 0 ; i < slides.length ; i++) {
+            const dot = document.createElement('li');
+            dot.setAttribute('data-slide-to', i + 1); 
+            dot.style.cssText = `   box-sizing: content-box;
+                                    flex: 0 1 auto;
+                                    width: 30px;
+                                    height: 6px;
+                                    margin-right: 3px;
+                                    margin-left: 3px;
+                                    cursor: pointer;
+                                    background-color: #34fcfb;
+                                    background-clip: padding-box;
+                                    border-top: 10px solid transparent;
+                                    border-bottom: 10px solid transparent;
+                                    opacity: .5;
+                                    transition: opacity .6s ease; ` ;
+            if(i == 0) {
+                dot.style.opacity = 1 ;
+            }
+            indicators.append(dot);
+            dots.push(dot);
+            
+        }
+
+
+
         nextSlide.addEventListener('click', () => {
             if(offset == +width.slice(0, width.length - 2) * (slides.length - 1) ) { //500px
                 offset = 0;
@@ -484,6 +528,9 @@ window.addEventListener('DOMContentLoaded' , ()=>{
                 }else {
                     currentSlidesNum.textContent = slideIndex;
                 }
+            
+                dots.forEach(dot => dot.style.opacity = '.5');
+                dots[slideIndex -1].style.opacity = 1 ;
         });
 
         previousSlide.addEventListener('click', () => {
@@ -506,8 +553,32 @@ window.addEventListener('DOMContentLoaded' , ()=>{
                     }else {
                        currentSlidesNum.textContent = slideIndex;
                         }
+
+             dots.forEach(dot => dot.style.opacity = '.5');
+             dots[slideIndex -1 ].style.opacity = 1 ;
            
         });
 
+        dots.forEach(dot => {
+            dot.addEventListener('click' , (e) => {
+                const slideTo = e.target.getAttribute('data-slide-to');
+
+                slideIndex = slideTo ;
+                offset = +width.slice(0, width.length - 2) * (slideTo - 1) ;
+                slidesField.style.transform = `translateX(-${offset}px)`;
+
+                if(slides.length < 10){
+                    currentSlidesNum.textContent =`0${slideIndex}`;
+                    }else {
+                        currentSlidesNum.textContent = slideIndex;
+                    }
+
+                dots.forEach(dot => dot.style.opacity = '.5');
+                dots[slideIndex -1].style.opacity = 1 ;
+
+            })
+        })
+
 
 });
+
